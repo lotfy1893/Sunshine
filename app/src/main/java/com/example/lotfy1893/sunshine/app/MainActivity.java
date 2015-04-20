62c13d8f9afb4a96906e7f8c11b316937d79a17f
lotfy1893 <mohamed.lotfy.hammad@gmail.com>
2015-04-20 23:55:20 +0200

    in lesson 4C

--- app/src/main/java/com/example/lotfy1893/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/lotfy1893/sunshine/app/MainActivity.java
@@ -1,41 +1,32 @@
 package com.example.lotfy1893.sunshine.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
-
 
 public class MainActivity extends ActionBarActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
-
-
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main  , menu);
         return true;
     }
 
@@ -48,39 +39,34 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
-            startActivity(new Intent(this,SettingsActivity.class));
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
         if (id == R.id.action_map) {
             openPreferredLocationInMap();
-                    return true;
-                   }
-
-
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
-
-
     private void openPreferredLocationInMap() {
+        String location = Utility.getPreferredLocation(this);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String location  = prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
 
-        Uri geolocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q",location).build();
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geolocation);
-        if(intent.resolveActivity(getPackageManager())!= null) {
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
         }
-        else {
-            Log.v("FetchWeatherForecast","couldn't call "+location);
-        }
-
-
     }
-
-
 }
